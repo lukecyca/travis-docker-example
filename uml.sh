7cb4637ff216afa2552d2e043288260063b33342
Luke Cyca <me@lukecyca.com>
2013-11-04 18:09:50 -0800

    Initial commit

---
+++ uml.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit on first error
+set -e
+
+# Kill background processes on exit
+trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
+
+# Start docker daemon
+docker -d &
+sleep 1
+
+# Use docker
+docker run ubuntu /bin/echo hello world
